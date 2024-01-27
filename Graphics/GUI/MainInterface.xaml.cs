5f2b52fe1d701f9f88c4eab4ea7787abdea39196
Nitro <ninolinda@hotmail.com>
2024-01-27 00:33:03 -0500

    Add project files.

---
+++ Graphics/GUI/MainInterface.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using URA_ColorBot.Static;
+
+namespace URA_ColorBot.Graphics.GUI;
+
+public partial class MainInterface : Window
+{
+    public MainInterface()
+    {
+        InitializeComponent();
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        // Set the top window bar into a "dark" mode
+        EnableNcRendering();
+    }
+
+    private void EnableNcRendering()
+    {
+        var value = true;
+
+        Interop.DwmSetWindowAttribute(
+            new System.Windows.Interop.WindowInteropHelper(this).Handle, 
+            20, ref value, Marshal.SizeOf(value)
+            );
+    }
+}
---
+++ Static/Interop.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace URA_ColorBot.Static;
+
+public static class Interop
+{
+    [DllImport("dwmapi.dll", PreserveSig = true)]
+    public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref bool attrValue, int attrSize);
+}
